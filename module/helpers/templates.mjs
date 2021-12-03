7e2ec4de4b4b548a2eaec0a1a56d7fe2a17324f7
Dan "Pan" Lott <danpan.nightshift@gmail.com>
2021-12-03 15:45:46 -0600

    Update templates.mjs

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -1,7 +1,5 @@
 export const preloadHandlebarsTemplates = async function()
 {
-    return loadTempaltes([
-        "systems/SalvageUnion/templates/actor/parts/actor-features.html",
-        "systems/SalvageUnion/templates/actor/parts/actor-items.html", 
+    return loadTemplates([
     ]);
 };
